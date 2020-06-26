66dd9d9afa80985de17471c5ececf34cf623c4a9
Carolina Oliveira <caroliveira.ti@gmail.com>
2020-06-26 13:47:30 -0300

    get repo by user/org

--- src/pages/GitRepositories.js
+++ src/pages/GitRepositories.js
@@ -1,31 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import MaterialTable from "material-table";
 import tableDefaults from "../utils/tableDefaults";
 import { AddCircle } from "@material-ui/icons";
+import { getRepo } from "../services";
 
-export default function GitRepositories() {
-  return (
-    <div  className="table">
-      <MaterialTable
-        {...tableDefaults()}
-        title="Tabela"
-        columns={[
-          { title: "Nome", field: "name" },
-          { title: "Sobrenome", field: "surname" },
-        ]}
-        data={[
-          {name: "Fulana", surname: "da Silva"},
-          {name: "Beltrana", surname: "Sauro"},
-        ]}
-        actions={[
-          {
-            icon: AddCircle,
-            tooltip: "Adicionar",
-            isFreeAction: true,
-            // onClick: async (evt, rowData) => 
-          },
-        ]}
-      />
-    </div>
-  );
+export default class GitRepositories extends Component {
+  state = { repo: [] };
+  async componentDidMount() {
+    this.setState({ repo: await getRepo("Caroliveira") });
+  }
+  render() {
+    console.log(this.state.repo);
+    return (
+      <div className="table">
+        <MaterialTable
+          {...tableDefaults()}
+          title="Tabela"
+          columns={[
+            { title: "Nome", field: "name" },
+            { title: "Sobrenome", field: "surname" },
+          ]}
+          data={[
+            { name: "Fulana", surname: "da Silva" },
+            { name: "Beltrana", surname: "Sauro" },
+          ]}
+          actions={[
+            {
+              icon: AddCircle,
+              tooltip: "Adicionar",
+              isFreeAction: true,
+              // onClick: async (evt, rowData) =>
+            },
+          ]}
+        />
+      </div>
+    );
+  }
 }
