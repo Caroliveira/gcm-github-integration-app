c7768c511292493eb30a2821a84feea955b476f0
Carolina Oliveira <caroliveira.ti@gmail.com>
2020-06-26 13:22:56 -0300

    add material table and icons

--- src/pages/GitRepositories.js
+++ src/pages/GitRepositories.js
@@ -1,9 +1,31 @@
 import React from "react";
+import MaterialTable from "material-table";
+import tableDefaults from "../utils/tableDefaults";
+import { AddCircle } from "@material-ui/icons";
 
 export default function GitRepositories() {
   return (
-    <div>
-      <h1>Git</h1>
+    <div  className="table">
+      <MaterialTable
+        {...tableDefaults()}
+        title="Tabela"
+        columns={[
+          { title: "Nome", field: "name" },
+          { title: "Sobrenome", field: "surname" },
+        ]}
+        data={[
+          {name: "Fulana", surname: "da Silva"},
+          {name: "Beltrana", surname: "Sauro"},
+        ]}
+        actions={[
+          {
+            icon: AddCircle,
+            tooltip: "Adicionar",
+            isFreeAction: true,
+            // onClick: async (evt, rowData) => 
+          },
+        ]}
+      />
     </div>
   );
 }
---
+++ src/utils/tableDefaults.js
@@ -0,0 +1,45 @@
+import {
+  Clear,
+  FirstPage,
+  LastPage,
+  ChevronRight,
+  ChevronLeft,
+  Search,
+  ArrowUpward,
+} from "@material-ui/icons";
+
+export default () => ({
+  options: {
+    actionsColumnIndex: -1,
+    sortable: true,
+  },
+  icons: {
+    FirstPage: FirstPage,
+    LastPage: LastPage,
+    NextPage: ChevronRight,
+    PreviousPage: ChevronLeft,
+    ResetSearch: Clear,
+    Search: Search,
+    SortArrow: ArrowUpward,
+  },
+  localization: {
+    body: {
+      emptyDataSourceMessage: "Não há dados disponíveis",
+    },
+    header: {
+      actions: "Ações",
+    },
+    pagination: {
+      labelDisplayedRows: `{from}-{to} de {count}`,
+      labelRowsSelect: "Linhas",
+      firstTooltip: "Primeira página",
+      previousTooltip: "Página anterior",
+      nextTooltip: "Próxima página",
+      lastTooltip: "Última página",
+    },
+    toolbar: {
+      searchTooltip: "Pesquisar",
+      searchPlaceholder: "Pesquisar",
+    },
+  },
+});
